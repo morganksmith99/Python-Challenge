4ba5eff0d5dc7099a4c44ef587d776abaf17eace
Morgan Smith <morgansmithedge@gmail.com>
2024-03-21 20:51:15 -0400

    parts 1-3 for budget and 1 for votes

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -119,4 +119,16 @@ with open('/Users/morgansmith/Desktop/Python-Challenge/PyBank/Resources/budget_d
 
 
 
+#PyPoll section
+#import the necessary libraries for reading csv files and writing to a text file
+with open('/Users/morgansmith/Desktop/Python-Challenge/PyPoll/Resources/election_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
+    #calculate the total number of votes cast
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+    total_votes = len(rows) - 1
+    print("Election Results")
+    print("----------------------------")
+    print("Total Votes:", total_votes)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,14 +13,6 @@ with open('/Users/morgansmith/Desktop/Python-Challenge/PyPoll/Resources/election
     print("----------------------------")
     print("Total Votes:", total_votes)
 
-#calculate A complete list of candidates who received votes, The percentage of votes each candidate won and, The total number of votes each candidate won 
-#the outcome should look like this:
-# -------------------------
-#Charles Casper Stockham: 23.049% (85213)
-#Diana DeGette: 73.812% (272892)
-#Raymon Anthony Doane: 3.139% (11606)
-#-------------------------
-
 
     
 
